8a1a81d1b6c37d01872708f4f79cb78ccf5f35c9
ThomasSpoelstra <Thomas_spoel@hotmail.com>
2025-05-15 16:31:37 +0200

    cleaning up unused use statements

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,7 +2,6 @@
 
 namespace App\Controller;
 
-use App\Model\Starship;
 use App\Repository\StarshipRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
